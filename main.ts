8445902c13c13fbaaf6114bf51fbc042438b3d0d
Guillermo Uscudum <68258148+uscudum@users.noreply.github.com>
2023-02-20 11:21:07 -0300

    Cambios actualizados

--- main.ts
+++ main.ts
@@ -1,12 +1,82 @@
 bluetooth.onBluetoothConnected(function () {
-    basic.showIcon(IconNames.Yes)
+    if (Temperatura < 9 || Temperatura > 30) {
+        basic.showLeds(`
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            . . . . #
+            `)
+    } else if (Humedad < 80) {
+        basic.showLeds(`
+            # # . # .
+            # . . . .
+            . # # # .
+            # . . . #
+            . . . . #
+            `)
+    } else if (Luminosidad < 20) {
+        basic.showLeds(`
+            # # . # #
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            . . . . #
+            `)
+    }
 })
 bluetooth.onBluetoothDisconnected(function () {
-    basic.showIcon(IconNames.No)
+    if (input.temperature() < 9 || input.temperature() > 30) {
+        basic.showLeds(`
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            . . . . #
+            `)
+    } else if (Humedad < 80) {
+        basic.showLeds(`
+            # # . # .
+            # . . . .
+            . # # # .
+            # . . . #
+            . . . . #
+            `)
+    } else if (Luminosidad < 20) {
+        basic.showLeds(`
+            # # . # #
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            . . . . #
+            `)
+    }
 })
+let Luminosidad = 0
+let Humedad = 0
+let Temperatura = 0
 basic.showIcon(IconNames.Heart)
 bluetooth.startTemperatureService()
 bluetooth.startIOPinService()
 basic.forever(function () {
-	
+    Temperatura = input.temperature()
+    Humedad = pins.analogReadPin(AnalogPin.P0)
+    Luminosidad = input.lightLevel()
+    basic.pause(5000)
 })
